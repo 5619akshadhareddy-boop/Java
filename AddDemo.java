d88fd1ee85ff43e046ac26716fe426ca34666151
5619akshadhareddy-boop <5619akshadhareddy@gmail.com>
2026-07-30 08:45:10 +0530

    Add files via upload

---
+++ AddDemo.java
@@ -0,0 +1,11 @@
+public class AddDemo {
+    public static void main(String [] args){
+        int b1=10;
+        int b2=30;
+        int result;
+        result=b1+b2;
+        System.out.println("the result is "+result);
+
+    }
+    
+}
